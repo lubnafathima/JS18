467d14ab88c1320298a51b7ba7a71d1947064f58
Lubna Fathima N <lubnafathima275@gmail.com>
2024-05-23 21:58:41 +0530

    init: complete code

---
+++ script.js
@@ -0,0 +1,13 @@
+// debugger;
+const obj = {
+  key1: 'value1',
+}
+
+// if("key1" in obj) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+console.log(obj?.key2);
+// console.log(obj.inner?.key2);
